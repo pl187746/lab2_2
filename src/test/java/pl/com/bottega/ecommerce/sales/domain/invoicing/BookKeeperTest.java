bafe73ee51c1c3f956fa1582560f1357a889d87c
Karol Kucharski <187746@edu.p.lodz.pl>
2016-04-14 19:14:29 +0200

    Refaktoryzacja testow.

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertThat;
 
 import java.math.BigDecimal;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
@@ -26,22 +27,26 @@ public class BookKeeperTest {
 
 	ProductData productData = new ProductData(null, null, null, ProductType.STANDARD, null);
 	
+	BookKeeper bookKeeper;
+	InvoiceRequest invoiceRequest;
+	TestTaxPolicy taxPolicy;
+	
+	@Before
+	public void setUp() {
+		bookKeeper = new BookKeeper(new InvoiceFactory());
+		invoiceRequest = new InvoiceRequest(null);
+		invoiceRequest.add(new RequestItem(productData, 1, new Money(BigDecimal.ONE)));
+		taxPolicy = new TestTaxPolicy();
+	}
+	
 	@Test
 	public void dlaZadaniaZJednymProduktemPodatekJestLiczonyRaz() {
-		BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
-		InvoiceRequest invoiceRequest = new InvoiceRequest(null);
-		invoiceRequest.add(new RequestItem(productData, 1, new Money(BigDecimal.ONE)));
-		TestTaxPolicy taxPolicy = new TestTaxPolicy();
 		bookKeeper.issuance(invoiceRequest, taxPolicy);
 		assertThat(taxPolicy.callCounter, is(1));
 	}
 	
 	@Test
 	public void dlaZadaniaZJednymProduktemFakturaZawieraJednaPozycje() {
-		BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
-		InvoiceRequest invoiceRequest = new InvoiceRequest(null);
-		invoiceRequest.add(new RequestItem(productData, 1, new Money(BigDecimal.ONE)));
-		TestTaxPolicy taxPolicy = new TestTaxPolicy();
 		Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
 		assertThat(invoice.getItems().size(), is(1));
 	}
